bdc69fb0b78c02a8a36aa4ca61928145aa9d89ea
Aman Raj <aman1105raj@gmail.com>
2024-03-02 04:02:28 +0530

    Update spline.jsx

--- src/components/spline.jsx
+++ src/components/spline.jsx
@@ -24,23 +24,23 @@ export default function App() {
             <div className='w-[25%] p-[50px]'>
                 <h1 className='text-[55px] font-bold'>Device Specification</h1>
                 <ul className='mt-10 list-disc list-inside font-bold text-xl'>
-                    <li>100% cotton fabric</li>
-                    <li>Durable with 100+ tests</li>
-                    <li>Soft and breathable material</li>
-                    <li>Easy to care for, machine washable</li>
-                    <li>Classic crew neckline</li>
-                    <li>Short sleeves for comfort</li>
-                    <li>Regular fit for a versatile look</li>
-                    <li>Available in various sizes and colors</li>
-                    <li>Great for everyday wear or casual occasions</li>
-                    <li>High-quality stitching for long-lasting wear</li>
+                <li><b>Weight:</b>206g</li>
+                    <li><b>Build:</b>glass front,stainless steel frame </li>
+                    <li><b>size:</b>6.1 inches</li>
+                    <li><b>OS:</b>iOS 16, upgradable to iOS 17.3.1</li>
+                    <li><b>Memory:</b>128GB 6GB RAM, 256GB 6GB RAM</li>
+                    <li><b>Camera:</b>Tripple(48MP+12MP+12MP)</li>
+                    <li><b>WLAN:</b>Wi-Fi 802.11 a/b/g/n/ac/6, dual-band</li>
+                    <li><b>Bluetooth:</b>5.3, A2DP, LE</li>
+                    <li><b>Bttery:</b>Li-Ion 3200 mAh, non-removable</li>
+                    <li><b>Performance:</b>AnTuTu: 968412 (v9)</li>
                 </ul>
             </div>
             <div className='w-[50%] h-screen'>
                 <Spline scene="https://prod.spline.design/tJrwAVWXEHJRXgU1/scene.splinecode" />
             </div>
             <div className='w-25%'>
-                <p className=" text-xl font-bold mb-4 text-center">Select Size</p>
+                <p className=" text-xl font-bold mb-4 text-center">Select Color (Available Soon..)</p>
                 <div className="flex justify-center">
                     <div className={`w-10 h-10 rounded-full bg-red-500 mx-2 flex items-center justify-center backdrop-blur-xl bg-opacity-40 ${selectedSize === 'XS' ? 'border-2 border-slate-200 ' : ''}`} onClick={() => handleSizeClick('XS')}>
                         <p className="text-white">XS</p>
